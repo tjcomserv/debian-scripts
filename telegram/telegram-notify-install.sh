56387b63948ea3ff9cb70f513d0c93d3fcad1fd1
TJ Computer Services <tjcomserv@users.noreply.github.com>
2023-03-19 08:17:12 +0000

    Update telegram-notify-install.sh

--- telegram/telegram-notify-install.sh
+++ telegram/telegram-notify-install.sh
@@ -4,10 +4,10 @@
 apt-get install curl
 
 # retrieve configuration file and main script
-wget -O /etc/telegram-notify.conf https://raw.githubusercontent.com/NicolasBernaerts/debian-scripts/master/telegram/telegram-notify.conf
-wget -O /usr/local/bin/telegram-notify https://raw.githubusercontent.com/NicolasBernaerts/debian-scripts/master/telegram/telegram-notify
+wget -O /etc/telegram-notify.conf https://raw.githubusercontent.com/tjcomserv/debian-scripts/master/telegram/telegram-notify.conf
+wget -O /usr/local/bin/telegram-notify https://raw.githubusercontent.com/tjcomserv/debian-scripts/master/telegram/telegram-notify
 chmod +x /usr/local/bin/telegram-notify
 
 # retrieve rsync wrapper
-wget -O /usr/local/bin/rsync https://raw.githubusercontent.com/NicolasBernaerts/debian-scripts/master/telegram/rsync
-chmod +x /usr/local/bin/rsync
+#wget -O /usr/local/bin/rsync https://raw.githubusercontent.com/NicolasBernaerts/debian-scripts/master/telegram/rsync
+#chmod +x /usr/local/bin/rsync
